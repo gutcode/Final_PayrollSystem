1f6c28b1eecce31bff00d994450a3eb93083fee7
Daniel Dahal <1991baburamdahal@gmail.com>
2018-04-02 00:52:59 +0200

    Create FullTime.java

---
+++ FullTime.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package payrollsystem;
+
+/**
+ *
+ * @author gutcode
+ */
+public class FullTime extends Employee{
+    private double salary;
+    private double overtime;
+       
+         public FullTime(int id, String name, double sal, double hourAndHalf, Vehicle vehicle)
+         {
+             super (id, name, vehicle);
+             this.overtime = hourAndHalf;
+             this.salary = sal;
+         }    
+    public double calculatePay() {
+        System.out.println("Full time employee.");
+        return (this.getSalary() + this.getOvertime());
+    }
+
+    /**
+     * @return the salary
+     */
+    public double getSalary() {
+        return salary;
+    }
+
+    /**
+     * @param salary the salary to set
+     */
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    /**
+     * @return the overtime
+     */
+    public double getOvertime() {
+        return overtime;
+    }
+
+    /**
+     * @param overtime the overtime to set
+     */
+    public void setOvertime(double overtime) {
+        this.overtime = overtime;
+    }
+    
+}
